5f2c8f6f2fe2deacbbcf514904e0abf741087dd4
mehmet1217 <mehmetaliakbas1@hotmail.com>
2023-02-25 18:06:52 +0100

    Test2 Uploaded

---
+++ src/Test/Test2_kekse.java
@@ -0,0 +1,7 @@
+package Test;
+
+public class Test2_kekse {
+    public static void main(String[] args) {
+        System.out.println("Bu class i ikinci branch ta yani kekse da olusturudk");
+    }
+}
